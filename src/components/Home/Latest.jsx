2d26d2b9f32db02e2b090882e5758dd144d03ab6
Arafat Shalehin <md.arafatshalehin@gmail.com>
2026-02-14 22:49:12 +0600

    Added Latest Section

---
+++ src/components/Home/Latest.jsx
@@ -0,0 +1,20 @@
+function LatestCard({ num }) {
+  return (
+    <div className="flex items-center gap-2 p-4 border rounded shadow-sm">
+      <h1 className="font-bold">Car Number</h1>
+      <span>{num}</span>
+    </div>
+  );
+}
+
+function Latest() {
+  return (
+    <div className="max-w-7xl mx-auto border flex flex-wrap gap-4 p-4">
+      {[1, 2, 3, 4, 5].map((num, index) => (
+        <LatestCard key={index} num={num} />
+      ))}
+    </div>
+  );
+}
+
+export default Latest;
